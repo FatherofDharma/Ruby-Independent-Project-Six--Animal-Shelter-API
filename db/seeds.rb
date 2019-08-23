5b6df8485564d20bb6a7ee837dc13b15948ca1d4
FatherofDharma <fatherofdharma@gmail.com>
2019-08-23 10:53:01 -0700

    add seeding code. still failing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+class Seed
+
+  def self.begin
+    Animal.destroy_all
+    seed = Seed.new
+    seed.make_animals_cats
+    seed.make_animals_dogs
+  end
+
+  def make_animals_cats
+    type = "Cat"
+    25.times do |i|
+      cat = Animal.create!(
+        name: Faker::Creature::Cat.name,
+        type: type,
+        age: rand(1..20),
+        breed: Faker::Creature::Cat.breed,
+        content: Faker::Quote.yoda
+      )
+      puts "Cat #{i} name is #{cat.name}, type is #{cat.type}, age is #{cat.age}, breed is #{cat.breed}, and the content is #{cat.content}"
+    end
+  end
+
+  def make_animals_dogs
+    type = "Dog"
+    25.times do |i|
+      dog = Animal.create!(
+        name: Faker::Creature::Dog.name,
+        type: type,
+        age: rand(1..20),
+        breed: Faker::Creature::Dog.breed,
+        content: Faker::Quote.yoda
+      )
+      puts "Dog #{i} name is #{dog.name}, type is #{dog.type}, age is #{dog.age}, breed is #{dog.breed}, and the content is #{dog.content}"
+    end
+  end
+end
+
+Seed.begin
