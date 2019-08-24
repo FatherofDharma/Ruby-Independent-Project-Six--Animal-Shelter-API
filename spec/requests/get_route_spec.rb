472241edf7843e7af6a86ac3aa8a5138c5e86ea0
FatherofDharma <fatherofdharma@gmail.com>
2019-08-23 18:22:05 -0700

    add passing spec to search by animal type.

--- spec/requests/get_route_spec.rb
+++ spec/requests/get_route_spec.rb
@@ -29,12 +29,22 @@ describe "get animal by id route", :type => :request do
     get "/v1/animals/#{animal.id}"
     expect(JSON.parse(response.body)["id"]).to eq(animal.id)
   end
+end
 
-  it 'returns a an animal by name' do
+describe "get animal by search route", :type => :request do
+  Animal.destroy_all
+  it 'returns animals by name' do
     animal = FactoryBot.create(:animal)
     animal2 = FactoryBot.create(:animal, :name => 'Buddy')
     get "/v1/animals", params: { :name => "Buddy"}
     expect(JSON.parse(response.body)[0]["name"]).to eq(animal2.name)
+  end
+
+  it 'returns animnals by animal type' do
+    animal = FactoryBot.create(:animal)
+    animal2 = FactoryBot.create(:animal, :animal_type => 'Cat')
+    get "/v1/animals", params: { :animal_type => "Cat"}
+    expect(JSON.parse(response.body)[0]["animal_type"]).to eq(animal2.animal_type)
   end
 
 end
