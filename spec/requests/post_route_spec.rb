421ab85abc4a6410e8930b0e17bf001a788fa4c3
FatherofDharma <fatherofdharma@gmail.com>
2019-08-23 18:48:01 -0700

    add passing post route specs for returning age, breed, and content.

--- spec/requests/post_route_spec.rb
+++ spec/requests/post_route_spec.rb
@@ -14,18 +14,18 @@ describe "post an animal route", :type => :request do
     expect(JSON.parse(response.body)['animal_type']).to eq('Cat')
   end
 
-  # it 'returns the animal age' do
-  #
-  # end
-  #
-  # it 'returns the animal breed' do
-  #
-  # end
-  #
-  # it 'returns the animal content' do
-  #
-  # end
-  #
+  it 'returns the animal age' do
+    expect(JSON.parse(response.body)['age']).to eq(7)
+  end
+
+  it 'returns the animal breed' do
+    expect(JSON.parse(response.body)['breed']).to eq('Calico')
+  end
+
+  it 'returns the animal content' do
+    expect(JSON.parse(response.body)['content']).to eq('I lick myself')
+  end
+
   it 'returns a created status' do
     expect(response).to have_http_status(:created)
   end
