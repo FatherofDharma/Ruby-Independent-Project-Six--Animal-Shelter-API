c03c94128a9edf3484fcfb62e01686d3d85ea9ff
FatherofDharma <fatherofdharma@gmail.com>
2019-08-23 19:02:43 -0700

    add passing specs for missing fields when posting animals route.

--- spec/requests/post_route_spec.rb
+++ spec/requests/post_route_spec.rb
@@ -31,3 +31,21 @@ describe "post an animal route", :type => :request do
   end
 
 end
+
+describe "post an animal route with missing content", :type => :request do
+  before do
+    Animal.destroy_all
+  end
+
+it 'returns correct error for empty field' do
+  post '/v1/animals', params: { :name => "", :animal_type => "Cat", :age => "7", :breed => "Calico", :content => "I lick myself" }
+  expect(response).to have_http_status(:unprocessable_entity)
+end
+
+it 'returns correct error for empty field' do
+  post '/v1/animals', params: { :name => "", :animal_type => "", :age => "", :breed => "", :content => "" }
+  expect(JSON.parse(response.body)['message']).to eq("Validation failed: Name can't be blank, Animal type can't be blank, Age can't be blank, Age is not included in the list, Breed can't be blank, Content can't be blank")
+end
+
+
+end
